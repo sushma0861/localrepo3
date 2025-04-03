ec9f628950083d401f15c21faa8fed6abd273aab
sushma0861 <sushmairanna6@gmail.com>
2025-04-03 20:16:21 +0530

    saved

--- CollegeHoliday.java
+++ CollegeHoliday.java
@@ -1,5 +1,6 @@
 public class CollegeHoliday {
     public static void main(String[] args) {
         System.out.println("College is holiday tomorrow");
+        System.out.println("College is there")
     }
 }
